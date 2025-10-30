384344e29c34b4f68e3c4aa87dff557c85c9382e
CherryPainter <sqy3258731070@163.com>
2025-10-30 21:31:27 +0800

    更新版本号为v1.7.0

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from tkinter import messagebox
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # 版本信息
-VERSION = "v1.6.3"
+VERSION = "v1.7.0"
 
 from logger import log_info, log_error
 
